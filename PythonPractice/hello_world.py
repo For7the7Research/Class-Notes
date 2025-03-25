eb29220a0aada901cac3b4646d6389ec68958a19
Ayana Scott-Elliston <ayana.scottelliston@gmail.com>
2025-03-24 21:52:04 -0500

    All of my pythonpractice notes.

---
+++ PythonPractice/hello_world.py
@@ -0,0 +1,12 @@
+#comments are ignored by the interpreter
+#can use for personal notes and can use to temporarily enable or disable lines in code
+# Use Control +(and) / to toggel commenting on a line
+print("Hello, world! WOW, A CHANGE")
+name = input("Name: ")
+# name = "Billy"
+other_name = "Billy"
+# print("Nice to meet you, " + name + "!")
+print("Nice to meet you, " + name + "! I am " + other_name)
+
+num= input("Number: ")
+num =int(num)
